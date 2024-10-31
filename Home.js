c725162d2b55a9f538462fe378893a74453ba908
MuhammedHafeez <165685916+hafeezn881@users.noreply.github.com>
2024-10-31 11:02:24 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <div>
+            <h1>Welcome to Sanpoly</h1>
+            <p>Your gateway to quality technical education</p>
+        </div>
+    );
+}
+export default Home;
